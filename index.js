79a74ee8e751954edc7648662fe8962a85f949d6
gaidenshinji <gaidenshinji2468@gmail.com>
2023-02-13 09:44:58 -0400

    Servidor basico

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("It's ok!");//DEBUG
+// configurar express
+const express = require("express");
+const app = express();
+
+// configurar puerto
+const port = 5000;
+
+// asociar un proceso a un endpoint, mediante un metodo http
+app.get("/", (req, res) => {
+    res.send("Hello this is my server");
+});
+
+// hacer escuchar el servidor a un puerto
+app.listen(port, () => {
+    console.log(`Server runs on port ${port}`);
+});
